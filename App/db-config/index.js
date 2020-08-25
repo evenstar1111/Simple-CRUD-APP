1211ff1cdd06000327e8543635a51e02156939cf
evenstar1111 <skshahil35@gmail.com>
2020-08-25 22:23:49 +0530

    Delete index.js

--- App/db-config/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-   user: 'sahil',
-   password: 'c3000b4000',
-   connString: `mongodb+srv://${this.user}:${this.password}@cluster0.q0uvf.mongodb.net/<dbname>?retryWrites=true&w=majority`
-}
